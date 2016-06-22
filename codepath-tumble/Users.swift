3dc4147f21c8b1121ec0a9a4cf2a783acc398356
d0wolfe <dwolfe@walmartlabs.com>
2016-06-22 16:25:32 -0700

    Stub out user data file

---
+++ codepath-tumble/Users.swift
@@ -0,0 +1,252 @@
+//
+//  Users.swift
+//  codepath-tumble
+//
+//  Created by David Wolfe on 6/22/16.
+//  Copyright © 2016 red58 Communications. All rights reserved.
+//
+
+import Foundation
+
+var users: [[String:Any]] = [
+    [
+        "name": "Bell",
+        "age": 24,
+        "job": "QA engineer at Instamotor",
+        "education": "",
+        "location": "",
+        "distance": "1.4 miles"
+    ],
+    [
+        "name": "Bram",
+        "age": 28,
+        "job": "Sales Director at Periscope",
+        "education": "",
+        "location": "",
+        "distance": "1.4 miles"
+    ],
+    [
+        "name": "Cale",
+        "age": "",
+        "job": "",
+        "education": "",
+        "location": "",
+        "distance": ""
+    ],
+    [
+        "name": "Sonja",
+        "age": "",
+        "job": "",
+        "education": "",
+        "location": "",
+        "distance": ""
+    ],
+    [
+        "name": "Noel",
+        "age": "",
+        "job": "",
+        "education": "",
+        "location": "",
+        "distance": ""
+    ],
+    [
+        "name": "Noel",
+        "age": "",
+        "job": "",
+        "education": "",
+        "location": "",
+        "distance": ""
+    ],
+    [
+        "name": "Noel",
+        "age": "",
+        "job": "",
+        "education": "",
+        "location": "",
+        "distance": ""
+    ],
+    [
+        "name": "Noel",
+        "age": "",
+        "job": "",
+        "education": "",
+        "location": "",
+        "distance": ""
+    ],
+    [
+        "name": "Noel",
+        "age": "",
+        "job": "",
+        "education": "",
+        "location": "",
+        "distance": ""
+    ],
+    [
+        "name": "Noel",
+        "age": "",
+        "job": "",
+        "education": "",
+        "location": "",
+        "distance": ""
+    ],
+    [
+        "name": "Noel",
+        "age": "",
+        "job": "",
+        "education": "",
+        "location": "",
+        "distance": ""
+    ],
+    [
+        "name": "Noel",
+        "age": "",
+        "job": "",
+        "education": "",
+        "location": "",
+        "distance": ""
+    ],
+    [
+        "name": "Noel",
+        "age": "",
+        "job": "",
+        "education": "",
+        "location": "",
+        "distance": ""
+    ],
+    [
+        "name": "Noel",
+        "age": "",
+        "job": "",
+        "education": "",
+        "location": "",
+        "distance": ""
+    ],
+    [
+        "name": "Noel",
+        "age": "",
+        "job": "",
+        "education": "",
+        "location": "",
+        "distance": ""
+    ],
+    [
+        "name": "Noel",
+        "age": "",
+        "job": "",
+        "education": "",
+        "location": "",
+        "distance": ""
+    ],
+    [
+        "name": "Noel",
+        "age": "",
+        "job": "",
+        "education": "",
+        "location": "",
+        "distance": ""
+    ],
+    [
+        "name": "Noel",
+        "age": "",
+        "job": "",
+        "education": "",
+        "location": "",
+        "distance": ""
+    ],
+    [
+        "name": "Noel",
+        "age": "",
+        "job": "",
+        "education": "",
+        "location": "",
+        "distance": ""
+    ],
+    [
+        "name": "Noel",
+        "age": "",
+        "job": "",
+        "education": "",
+        "location": "",
+        "distance": ""
+    ],
+    [
+        "name": "Noel",
+        "age": "",
+        "job": "",
+        "education": "",
+        "location": "",
+        "distance": ""
+    ],
+    [
+        "name": "Noel",
+        "age": "",
+        "job": "",
+        "education": "",
+        "location": "",
+        "distance": ""
+    ],
+    [
+        "name": "Noel",
+        "age": "",
+        "job": "",
+        "education": "",
+        "location": "",
+        "distance": ""
+    ],
+    [
+        "name": "Noel",
+        "age": "",
+        "job": "",
+        "education": "",
+        "location": "",
+        "distance": ""
+    ],
+    [
+        "name": "Noel",
+        "age": "",
+        "job": "",
+        "education": "",
+        "location": "",
+        "distance": ""
+    ],
+    [
+        "name": "Noel",
+        "age": "",
+        "job": "",
+        "education": "",
+        "location": "",
+        "distance": ""
+    ],
+    [
+        "name": "Noel",
+        "age": "",
+        "job": "",
+        "education": "",
+        "location": "",
+        "distance": ""
+    ],
+    [
+        "name": "Noel",
+        "age": "",
+        "job": "",
+        "education": "",
+        "location": "",
+        "distance": ""
+    ],
+    [
+        "name": "Noel",
+        "age": "",
+        "job": "",
+        "education": "",
+        "location": "",
+        "distance": ""
+    ],
+    [
+        "name": "Noel",
+        "age": "",
+        "job": "",
+        "education": "",
+        "location": "",
+        "distance": ""
+    ]
+]
