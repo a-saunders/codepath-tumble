87fc8e803da10904319f2f777c8da508e2068a2d
sdaviswalmart <sdavi11@l-sb8j552g8w-m.homeoffice.wal-mart.com>
2016-06-23 12:56:54 -0700

    Swipe Pan Interaction

--- codepath-tumble/SwipeViewController.swift
+++ codepath-tumble/SwipeViewController.swift
@@ -11,7 +11,71 @@ import UIKit
 class SwipeViewController: UIViewController {
 
     @IBOutlet weak var cardView: UIView!
+    @IBOutlet var cardPanRecognizer: UIPanGestureRecognizer!
+    @IBOutlet weak var imageView: UIScrollView!
+    
+    var cardInitialCenter: CGPoint!
 
+
+
+    @IBAction func didPanCard(sender: UIPanGestureRecognizer) {
+        
+        // Absolute (x,y) coordinates in card view
+        var point = cardPanRecognizer.locationInView(view)
+        
+        // Relative change in (x,y) coordinates from where gesture began.
+        var translation = cardPanRecognizer.translationInView(view)
+        var velocity = cardPanRecognizer.velocityInView(view)
+        
+        
+        if sender.state == UIGestureRecognizerState.Began {
+            cardInitialCenter = cardView.center
+
+            
+        } else if sender.state == UIGestureRecognizerState.Changed {
+            
+            // If pan input x value changes, move Message View
+            cardView.center.x = cardInitialCenter.x + translation.x
+            cardView.center.y = cardInitialCenter.y + translation.y
+            
+            if translation.x > 0 {
+                UIView.animateWithDuration(0.2, animations: {
+                    self.cardView.transform = CGAffineTransformMakeRotation(CGFloat(11.25 * M_PI / 180))
+                })
+            }
+                
+            else if translation.x < 0 {
+                UIView.animateWithDuration(0.2, animations: {
+                    self.cardView.transform = CGAffineTransformMakeRotation(CGFloat(-11.25 * M_PI / 180))
+                })
+            }
+            
+        } else if sender.state == UIGestureRecognizerState.Ended {
+                
+                // move messageImage back to original point
+                
+                if (abs(translation.x) <= 100) {
+                    UIView.animateWithDuration(0.4, animations: {
+                        self.cardView.center.x = self.cardInitialCenter.x
+                        self.cardView.center.y = self.cardInitialCenter.y
+                        self.cardView.transform = CGAffineTransformIdentity
+                    })
+                    
+                } else if translation.x > 100 {
+                    UIView.animateWithDuration(0.4, animations: {
+                        self.cardView.transform = CGAffineTransformMakeTranslation(400,0)
+                    })
+                    
+                }
+                    
+                else if translation.x < 100 {
+                    UIView.animateWithDuration(0.4, animations: {
+                        self.cardView.transform = CGAffineTransformMakeTranslation(-400,0)
+                    })
+                }
+        }
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +84,10 @@ class SwipeViewController: UIViewController {
         cardView.layer.borderColor = UIColor(red: 225/255, green: 225/255, blue: 225/255, alpha: 1).CGColor
         cardView.layer.cornerRadius = 10;
         cardView.layer.masksToBounds = true;
+        
+        imageView.layer.cornerRadius = 5;
+
+       
 
         
         // Do any additional setup after loading the view.
